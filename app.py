7777f4213c870fbe4067930a72b5529fd0f4c491
caoruoxuan <2100017739@stu.pku.edu.cn>
2026-04-14 17:39:29 +0200

    Fix Streamlit Cloud root page routing

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import streamlit as st
 
-from survey_app.shared import bootstrap_root_app, go_to_page
+from survey_app.shared import bootstrap_root_app, go_to_page, render_consent_page
 
 
 bootstrap_root_app()
-go_to_page(int(st.session_state.get("page", 0)))
+current_page = int(st.session_state.get("page", 0))
+
+if current_page == 0:
+	render_consent_page()
+else:
+	go_to_page(current_page)
