21d562794ab1181e8d1a5dfe7d65b5573cf74709
caoruoxuan <2100017739@stu.pku.edu.cn>
2026-04-14 17:42:19 +0200

    Remove root auto-navigation

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 import streamlit as st
 
-from survey_app.shared import bootstrap_root_app, go_to_page, render_consent_page
+from survey_app.shared import bootstrap_root_app, render_consent_page
 
 
 bootstrap_root_app()
-current_page = int(st.session_state.get("page", 0))
-
-if current_page == 0:
-	render_consent_page()
-else:
-	go_to_page(current_page)
+st.session_state.page = 0
+render_consent_page()
